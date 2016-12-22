6cf19f11b3c2e352a208a9e02b4b7a666f4398da
Paul Piper <pp@ilscipio.com>
2016-12-22 14:47:38 +0000

    Reverted revision(s) 14054 from scipioaddons/branches/bootstrap-latest: NoRef *Bootstrap fixes*
    * Set row container to class .row-fluid to fix overlapping issue
    ........
    
    
    git-svn-id: svn://data.ilscipio.com/ilscipio/scipioaddons/branches/bootstrap-latest@14055 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- includes/themeStyles.groovy
+++ includes/themeStyles.groovy
@@ -602,7 +602,7 @@ context.styles = [
     "elem_color_error" : "colored-elem danger",
     
   /* Grid */
-    "grid_row" : "row-fluid",
+    "grid_row" : "row",
     "grid_cell" : "columns",
     "grid_cell_default" : "col-lg-12",
     "grid_display" : "display",      /* display grid */
