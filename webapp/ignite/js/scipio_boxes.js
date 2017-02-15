363a03a54d3d946ce88f439bb48526c818197949
Paul Piper <pp@ilscipio.com>
2017-02-15 16:24:04 +0000

    Refs #1172
    * Removed duplicated inclusion of bootstrap styles (included in styles.css)
    * Added scipio_boxes.js
    
    git-svn-id: svn://data.ilscipio.com/ilscipio/scipioaddons/branches/ignite-shop-latest@14745 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

---
+++ webapp/ignite/js/scipio_boxes.js
@@ -0,0 +1,23 @@
+/**
+ * SCIPIO: jquery plugin to add a class to subsets of html elements.
+ * Used to add classes to elements, so that they can be styled as boxes.
+ * */
+var elSelector = 'pre.scrollable,.section-screenlet-content';
+var notSubSelector = 'pre.scrollable,.section-screenlet-content, table,.orbit-container, .steps, .tile-container, .signup-panel,.button-group,.trumbowyg-box';
+var scipioBoxClass = 'card';
+var scipioBoxSubClass = 'card';
+
+
+function scipio_boxify(){
+    $(elSelector).each(function(){
+        if($(this).has(notSubSelector).length == 0 && $(this).text().trim().length > 0){
+            $(this).addClass(scipioBoxClass);
+            //$(this).children('ul')
+        }
+    });
+}
+
+$(function(){
+    scipio_boxify();
+});
+
