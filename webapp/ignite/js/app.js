78092a0894d4443e39cf0aca5db59c368012d425
Paul Piper <pp@ilscipio.com>
2017-03-13 17:07:26 +0000

    NoRef
    *  Added thumbnail gallery
    
    git-svn-id: svn://data.ilscipio.com/ilscipio/scipioaddons/branches/ignite-shop-latest@15107 55bbc10b-e964-4c8f-a844-a62c6f7d3c80

--- webapp/ignite/js/app.js
+++ webapp/ignite/js/app.js
@@ -611,6 +611,204 @@ function scipioObjectFit(){
     return false;
 }
 
+/** 
+ * Dynamic Modal
+ * 
+ * Based on: https://github.com/michaelsoriano/bootstrap-photo-gallery
+ * 
+ * Simplified & removed anchor click-event
+ * **/
+$.fn.bsPhotoGallery = function(options) {
+
+    var settings = $.extend({}, $.fn.bsPhotoGallery.defaults, options);
+    var id = generateId();
+    var clicked = {};
+
+    function getCurrentUl(){
+      return 'ul[data-bsp-ul-id="'+clicked.ulId+'"][data-bsp-ul-index="'+clicked.ulIndex+'"]';
+    }
+    function generateId() {
+      //http://fiznool.com/blog/2014/11/16/short-id-generation-in-javascript/
+      var ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      var ID_LENGTH = 4;
+      var out = '';
+      for (var i = 0; i < ID_LENGTH; i++) {
+        out += ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length));
+      }
+      return 'bsp-'+out;
+    }
+    function createModalWrap(){
+
+      if($('#bsPhotoGalleryModal').length !== 0){
+        return false;
+      }
+
+      var modal = '';
+      modal += '<div class="modal fade" id="bsPhotoGalleryModal" tabindex="-1" role="dialog"';
+      modal += 'aria-labelledby="myModalLabel" aria-hidden="true">';
+      modal += '<div class="modal-dialog modal-lg"><div class="modal-content">';
+      modal += '<div class="modal-body"></div></div></div></div>';
+      $('body').append(modal);
+
+    }
+    function showHideControls(){
+  		var total = $(getCurrentUl()+' li[data-bsp-li-index]').length;
+
+  		if(total === clicked.nextImg){
+  			$('a.next').hide();
+  		}else{
+  			$('a.next').show()
+  		}
+  		if(clicked.prevImg === -1){
+  			$('a.previous').hide();
+  		}else{
+  			$('a.previous').show()
+  		}
+  	}
+    function showModal(){
+
+        var src = $(this).find('img').attr('src');
+        var largeImg = $(this).find('img').attr('data-bsp-large-src');
+        if(typeof largeImg === 'string'){
+              src = largeImg;
+        }
+        var index = $(this).attr('data-bsp-li-index');
+        var ulIndex = $(this).parent('ul').attr('data-bsp-ul-index');
+        var ulId = $(this).parent('ul').attr('data-bsp-ul-id');
+        var theImg = $(this).find('img');
+        var pText = $(this).find('.text').html();        
+        var modalText = typeof pText !== 'undefined' ? pText : 'undefined';
+        var alt =  typeof theImg.attr('alt') == 'string' ? theImg.attr('alt') : null;
+        
+        clicked.img = src;
+        clicked.prevImg = parseInt(index) - parseInt(1);
+    		clicked.nextImg = parseInt(index) + parseInt(1);
+        clicked.ulIndex = ulIndex;
+        clicked.ulId = ulId;
+
+
+        $('#bsPhotoGalleryModal').modal();
+
+        var html = '';
+        var img = '<img src="' + clicked.img + '" class="img-responsive"/>';
+
+        html += img;
+        html += '<span class="' + settings.iconClose + ' bsp-close"></span>';
+        html += '<div class="bsp-text-container">';
+        
+        if(alt !== null){
+          html += '<h6>'+alt+'</h6>'
+        }
+        if(typeof pText !== 'undefined'){
+          html += '<p class="pText">'+pText+'</p>'
+        }        
+        html += '</div>';
+        html += '<a class="bsp-controls next" data-bsp-id="'+clicked.ulId+'" href="'+ (clicked.nextImg) + '"><span class="' + settings.iconRight + '"></span></a>';
+        html += '<a class="bsp-controls previous" data-bsp-id="'+clicked.ulId+'" href="' + (clicked.prevImg) + '"><span class="' + settings.iconLeft + '"></span></a>';
+      
+        $('#bsPhotoGalleryModal .modal-body').html(html);
+        $('.bsp-close').on('click', closeModal);
+        showHideControls();
+    }
+
+    function closeModal(){
+      $('#bsPhotoGalleryModal').modal('hide');
+    }
+
+    function nextPrevHandler(){
+
+        var ul = $(getCurrentUl());
+        var index = $(this).attr('href');
+
+        var src = ul.find('li[data-bsp-li-index="'+index+'"] img').attr('src');
+        var largeImg = ul.find('li[data-bsp-li-index="'+index+'"] img').attr('data-bsp-large-src');
+        if(typeof largeImg === 'string'){
+              src = largeImg;
+        } 
+        
+        var pText = ul.find('li[data-bsp-li-index="'+index+'"] .text').html();        
+        var modalText = typeof pText !== 'undefined' ? pText : 'undefined';
+        var theImg = ul.find('li[data-bsp-li-index="'+index+'"] img');
+        var alt =  typeof theImg.attr('alt') == 'string' ? theImg.attr('alt') : null;
+         
+        $('#bsPhotoGalleryModal .modal-body img').attr('src', src);
+        var txt = '';
+        if(alt !== null){
+          txt += '<h6>'+alt+'</h6>'
+        }
+        if(typeof pText !== 'undefined'){
+          txt += '<p class="pText">'+pText+'</p>'
+        }        
+        
+        $('.bsp-text-container').html(txt); 
+
+        clicked.prevImg = parseInt(index) - 1;
+        clicked.nextImg = parseInt(clicked.prevImg) + 2;
+
+        if($(this).hasClass('previous')){
+            $(this).attr('href', clicked.prevImg);
+            $('a.next').attr('href', clicked.nextImg);
+        }else{
+            $(this).attr('href', clicked.nextImg);
+            $('a.previous').attr('href', clicked.prevImg);
+        }
+        // console.log(clicked);
+      showHideControls();
+      return false;
+    }
+    function clearModalContent(){
+      $('#bsPhotoGalleryModal .modal-body').html('');
+      clicked = {};
+    }
+
+
+
+    this.each(function(i){
+      //ul
+      var items = $(this).find('li');
+      $(this).attr('data-bsp-ul-id', id);
+      $(this).attr('data-bsp-ul-index', i);
+
+      items.each(function(x){
+    	$(this).find('a').on("click", function (e) {
+            e.preventDefault();
+        }); 
+        var theImg = $(this).find('img'); 
+        $(this).attr('data-bsp-li-index', x);
+        theImg.addClass('img-responsive');
+        if(settings.fullHeight){
+          theImg.wrap('<div class="imgWrapper"></div>')
+        }
+        if(settings.hasModal === true){
+          $(this).addClass('bspHasModal');
+          $(this).on('click', showModal);
+        }
+      });
+    })
+
+    if(settings.hasModal === true){
+      //this is for the next / previous buttons
+      $(document).on('click', 'a.bsp-controls[data-bsp-id="'+id+'"]', nextPrevHandler);
+      $(document).on('hidden.bs.modal', '#bsPhotoGalleryModal', clearModalContent);
+      //start init methods
+      createModalWrap();
+    }
+
+    return this;
+};
+/*defaults*/
+$.fn.bsPhotoGallery.defaults = {
+  'hasModal' : true, 
+  'fullHeight' : true,
+  'iconClose' : 'glyphicon glyphicon-remove-circle',
+  'iconLeft' : 'glyphicon glyphicon-chevron-left',
+  'iconRight' : 'glyphicon glyphicon-chevron-right'
+}
+
+$('ul.clearing-thumbs').bsPhotoGallery({});
+
+
+// checkboxToFormHiddenInput
 
 function checkboxToFormHiddenInput(checkboxElem, formSel) {
     jQuery(formSel + ' input[name="' + checkboxElem.name + '"]').remove();
